6c5c8e9b28119dfc3b77f28793294d81491fcf71
Chandra <1030819289@qq.com>
2023-04-08 19:40:47 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 import { serve } from "https://deno.land/std@0.181.0/http/server.ts";
 
-const OPENAI_API_HOST = "https://api.openai.com";
+const OPENAI_API_HOST = "api.openai.com";
 
 serve(async (request) => {
   const url = new URL(request.url);
